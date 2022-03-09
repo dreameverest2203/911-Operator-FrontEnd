ae4a80d4f6799de6f1331b1c8d3c4608fdc8873b
Rajas Bansal <rajasbansal1998@gmail.com>
2022-03-08 20:26:49 -0800

    Changed transcription to new lines

--- 911-operator-interface/src/App.js
+++ 911-operator-interface/src/App.js
@@ -1,47 +1,137 @@
 import axios from 'axios';
- 
+
 import React,{Component} from 'react';
+import Recorder from 'recorder-js';
 import './App.css';
 import Button from '@material-ui/core/Button';
 import MapContainer from './MapContainer';
 
+var recorder = null;
+var audioStream = null;
+
 class App extends Component {
-  
+  constructor(props) {
+    super(props);
+    this.mic = React.createRef();
+
+    this.accessMic = this.accessMic.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.handleSuccess = this.handleSuccess.bind(this);
+
+    this.stopAccessingMic = this.stopAccessingMic.bind(this);
+    this.getTextFromGoogle = this.getTextFromGoogle.bind(this);
+
+  }
+
     state = {
- 
+
       // Initially, no file is selected
       selectedFile: null,
       transcription: null,
       lat: 37.4275,
       lng: -122.1697,
       raw_ner: null,
-      emergency: null
+      emergency: null,
+      isMicActive: false
     };
-    
+
+    accessMic() {
+      if (!this.state.isMicActive) {
+        return;
+      }
+
+      const audioContext = new (window.AudioContext ||
+        window.webkitAudioContext)();
+
+      recorder = new Recorder(audioContext);
+
+      navigator.mediaDevices
+        .getUserMedia({ audio: true })
+        .then(this.handleSuccess)
+        .catch(err => console.log('Uh oh... unable to get stream...', err));
+
+      setTimeout(() => {this.stopAccessingMic(); this.accessMic()}, 5000);
+    }
+
+    handleSuccess(stream) {
+      audioStream = stream;
+
+      recorder.init(stream);
+      recorder.start();
+    }
+
+    getTextFromGoogle(blob) {
+      let filename = new Date().toISOString();
+      let xhr = new XMLHttpRequest();
+      xhr.onload = function(e) {
+        if (this.readyState === 4) {
+          console.log(e.target.responseText);
+        }
+      };
+      let formData = new FormData();
+      const myFile = new File([blob], filename, {type: blob.type});
+      formData.append('myFile', myFile, filename);
+      formData.append('noPunctuation', true)
+      xhr.open('POST', 'http://localhost:5000/transcribe', true);
+      xhr.send(formData);
+    }
+
+    handleClick() {
+      const isMicActive = this.state.isMicActive;
+
+      this.state.isMicActive = !isMicActive;
+
+      if (!isMicActive) {
+        this.checkPermissions();
+        this.accessMic();
+      } else {
+        this.stopAccessingMic();
+      }
+    }
+
+    stopAccessingMic() {
+      audioStream && audioStream.getTracks()[0].stop();
+      recorder.stop().then(({ blob, buffer }) => {
+        this.getTextFromGoogle(blob);
+      });
+    }
+
+    checkPermissions() {
+      navigator.permissions
+        .query({ name: 'microphone' })
+        .then(permissionObj => {
+          console.log('Permission status - ', permissionObj.state);
+        })
+        .catch(error => {
+          console.log('Permission status - Got error :', error);
+        });
+    }
+
     // On file select (from the pop up)
     onFileChange = event => {
-    
+
       // Update the state
       this.setState({ selectedFile: event.target.files[0] });
-    
+
     };
-    
+
     // On file upload (click the upload button)
     onFileUpload = () => {
-    
+
       // Create an object of formData
       const formData = new FormData();
-    
+
       // Update the formData object
       formData.append(
         "myFile",
         this.state.selectedFile,
         this.state.selectedFile.name
       );
-    
+
       // Details of the uploaded file
       console.log(this.state.selectedFile);
-    
+
       // Request made to the backend api
       // Send formData object
       axios.post('http://localhost:5000/transcribe', formData)
@@ -79,27 +169,27 @@ class App extends Component {
             }
           )
         }
-      });  
+      });
     };
 
     fileData = () => {
-    
+
       if (this.state.selectedFile) {
         return (
           <div>
             <h2 className='center'>File Details:</h2>
-             
+
               <p className='center'>File Name: {this.state.selectedFile.name}</p>
-              
-                          
+
+
               <p className='center'>File Type: {this.state.selectedFile.type}</p>
-              
-                          
+
+
               <p className='center'>
               Last Modified:{" "}
               {this.state.selectedFile.lastModifiedDate.toDateString()}
             </p>
- 
+
           </div>
         );
       } else {
@@ -113,14 +203,14 @@ class App extends Component {
     };
 
     getTranscription = () => {
-    
+
       if (this.state.transcription) {
         return (
           <div>
             <h2 className='center'>File Transcription:</h2>
-             
-              <p className='center' id='text-margin'>{this.state.transcription}</p>
- 
+              <div className='new-line center'>{this.state.transcription.replaceAll(".", "\n")}</div>
+              {/* <p className='center' id='text-margin'>{this.state.transcription.split(".").map((item) => {item}<br></br> )}</p> */}
+
           </div>
         );
       } else {
@@ -136,9 +226,9 @@ class App extends Component {
         return (
           <div>
               <h2 className='center'>Named Entities Found: </h2>
-              
+
               <p className='center' id='text-margin'>Address: {this.state.raw_ner.data.address}</p>
-  
+
           </div>
         )
       }
@@ -146,9 +236,9 @@ class App extends Component {
     getEmergency = () => {
       if (this.state.emergency) {
         return (
-          <div>              
+          <div>
               <p className='center' id='text-margin'>Emergency: {this.state.emergency}</p>
-  
+
           </div>
         )
       }
@@ -159,11 +249,24 @@ class App extends Component {
         <div className='center'>
           <MapContainer className='center' lat={this.state.lat} lng={this.state.lng} isMarkerShown={true} />
         </div>
-      ) 
+      )
     }
-    
+
+    getMicButton() {
+      return (<div
+        id='mic'
+        ref={this.mic}
+        onClick={this.handleClick}
+        className={
+          this.state.isMicActive ? 'mic-btn mic-btn-active' : 'mic-btn'
+        }
+      >
+        <button>Mic Check</button>
+      </div>);
+    }
+
     render() {
-    
+
       return (
         <div>
             <h1 className='center'>
@@ -187,9 +290,10 @@ class App extends Component {
           {this.getEmergency()}
           {this.getMap()}
           <br></br>
+          {this.getMicButton()}
         </div>
       );
     }
   }
- 
+
   export default App;
